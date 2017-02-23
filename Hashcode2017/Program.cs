c29dbaf627eb729f30f5fb3307f79e9fd8176535
Konstantinos Gkinis <raidenfreeman@hotmail.com>
2017-02-23 19:10:01 +0100

    Added output generator

--- Hashcode2017/Program.cs
+++ Hashcode2017/Program.cs
@@ -10,6 +10,7 @@ namespace Hashcode2017
     {
         static void Main(string[] args)
         {
+            OutputGenerator.CreateOutput("out", new List<List<int>> { new List<int> { 0, 2 }, new List<int> { 1, 3, 1 }, new List<int> { 2, 0, 1 } });
         }
     }
 }
